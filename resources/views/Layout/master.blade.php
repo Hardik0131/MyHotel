99f006b13d7b11e81491b305d8bd14d4d231484c
Hardik0131 <hardikbhaliya333@gmail.com>
2025-12-12 18:20:19 +0530

    Create a master page for Use Other Page

---
+++ resources/views/Layout/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.7.0/fonts/remixicon.css" rel="stylesheet" />
+    <title>@yield('title', 'Hotel')</title>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+        crossorigin="anonymous"></script>
+    @vite(['resources/css/app.css'])
+    @yield('vite')
+</head>
+
+<body>
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
