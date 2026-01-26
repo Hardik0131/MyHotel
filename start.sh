57af0288ba4e6ef026beefc918c4d72dc4e09f93
Hardik0131 <hardikbhaliya333@gmail.com>
2026-01-26 12:56:36 +0530

    Add start script for migration

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-==> Setting WEB_CONCURRENCY=1 by default, based on available CPUs in the instance
-==> Deploying...
-sh: 1: cd /var/www/html && /usr/local/bin/php artisan migrate --force && apache2-foreground: not found
-==> Exited with status 127
-==> Common ways to troubleshoot your deploy: https://render.com/docs/troubleshooting-deploys
-Menu
-sh: 1: cd /var/www/html && /usr/local/bin/php artisan migrate --force && apache2-foreground: not found
+#!/bin/sh
+
+cd /var/www/html
+
+/usr/local/bin/php artisan migrate --force
+
+exec apache2-foreground
