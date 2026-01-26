82af16a834ced4304a9586fef3dcdfd422f41b91
Hardik0131 <hardikbhaliya333@gmail.com>
2026-01-26 15:55:14 +0530

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,32 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js", "resources/css/login.css", "resources/js/login.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+
+                // admin assets
+                "resources/js/admin/sidebar.js",
+                "resources/js/admin/rooms.js",
+                "resources/js/admin/booking.js",
+                "resources/css/admin/layout/sidebar.css",
+                "resources/css/admin/layout/master.css",
+                "resources/css/admin/rooms.css",
+                "resources/css/admin/booking.css",
+
+                //visitor 
+                "resources/js/booking.js",
+                "resources/js/bootstrap.js",
+                "resources/css/booking.css",
+                "resources/css/contact.css",
+                "resources/css/details.css",
+                "resources/css/filterdroom.css",
+                "resources/css/home.css",
+                "resources/css/login.css",
+                "resources/css/nav.css",
+                "resources/css/register.css",
+                "resources/css/rooms.css",
+            ],
             refresh: true,
         }),
         tailwindcss(),
