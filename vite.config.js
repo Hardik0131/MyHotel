a7ba9947f2fda959983b68bcdda001b23a2d0d21
Hardik0131 <hardikbhaliya333@gmail.com>
2026-01-30 18:58:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,38 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', "resources/css/admin/layout/master.css", "resources/css/admin/layout/sidebar.css", "resources/css/admin/booking.css", "resources/css/admin/rooms.css", "resources/css/app.css", "resources/css/booking.css", "resources/css/contact.css", "resources/css/details.css", "resources/css/filterdroom.css", "resources/css/filterdroom.css", "resources/css/home.css", "resources/css/login.css", "resources/css/nav.css", "resources/css/register.css", "resources/css/rooms.css", "resources/js/admin/rooms.js", "resources/js/admin/sidebar.js", "resources/js/admin.js", "resources/js/app.js", "resources/js/booking.js", "resources/js/bootstrap.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/app.css",
+                "resources/css/booking.css",
+                "resources/css/contact.css",
+                "resources/css/details.css",
+                "resources/css/filterdroom.css",
+                "resources/css/filterdroom.css",
+                "resources/css/home.css",
+                "resources/css/login.css",
+                "resources/css/nav.css",
+                "resources/css/register.css",
+                "resources/css/rooms.css",
+                "resources/js/admin/rooms.js",
+                "resources/js/admin/sidebar.js",
+                "resources/js/admin.js",
+                "resources/js/app.js",
+                "resources/js/booking.js",
+                "resources/js/bootstrap.js",
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
